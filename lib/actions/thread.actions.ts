0cd2fdefef00b48f77ce2e113dc914a2c15fbe21
Naaasurfuture <119884782+CallmeRajul@users.noreply.github.com>
2024-03-02 22:50:41 +0700

    add update a litle

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -14,6 +14,9 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
   // Calculate the number of posts to skip based on the page number and page size.
   const skipAmount = (pageNumber - 1) * pageSize;
 
+  
+
+
   // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
   const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
     .sort({ createdAt: "desc" })
@@ -144,6 +147,10 @@ export async function deleteThread(id: string, path: string): Promise<void> {
       { _id: { $in: Array.from(uniqueAuthorIds) } },
       { $pull: { threads: { $in: descendantThreadIds } } }
     );
+
+
+    // nothing change
+    
 
     // Update Community model
     await Community.updateMany(
